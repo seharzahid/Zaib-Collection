d64fcb37c066b6846b914405b32b75411f3b8167
Sehar Zahid <seharkhan2028@gmail.com>
2026-06-17 01:34:48 +0500

    fixed dynamic static paths for vercel

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const app = express();
 const mongoose = require('mongoose');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
 
 const upload = multer({ dest: 'public/uploads/' });
 
