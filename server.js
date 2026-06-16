a077cfa3849a2f8307759b6418174dad4febf7d6
Sehar Zahid <seharkhan2028@gmail.com>
2026-06-17 01:23:58 +0500

    fixed port for vercel

--- server.js
+++ server.js
@@ -204,4 +204,7 @@ app.post('/api/place-order', (req, res) => {
         res.json({ success: true, message: "Order placed and email sent successfully!" });
     });
 });
-app.listen(3000, () => { console.log('Server running on port 3000'); });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+});
